6d7ff8fbb701ca7968aa86a8692038e3884198c5
Dan Wilson <dan@thedatacollective.com.au>
2022-11-18 14:30:12 +1000

    update data to simplify with only qld shapefile

---
+++ data-raw/map_shape.R
@@ -0,0 +1,9 @@
+library(sf)
+
+## import map shape
+sf_locality <- read_sf("data-raw/1270055003_lga_2020_aust_shp/LGA_2020_AUST.shp")
+
+## filter map shape to Queensland
+sf_qld <- sf_locality[sf_locality$STE_NAME16 == "Queensland", ]
+
+usethis::use_data(sf_qld, internal = TRUE, overwrite = TRUE)
