b8461a7e4e3f6c68197ca21005f0911c4378f349
Dan Wilson <dan@thedatacollective.com.au>
2022-11-18 15:31:56 +1000

    update to export sf_qld

--- R/data.R
+++ R/data.R
@@ -2,4 +2,5 @@
 #'
 #' @source Australian Bureau of Statistics - https://www.abs.gov.au/AUSSTATS/abs@.nsf/Lookup/1270.0.55.003Main+Features1June%202020?OpenDocument
 #' @return sf object
+#' @export
 "sf_qld"
