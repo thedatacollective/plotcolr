63f31bbb46fc42db4f205d073457b7b35b07960c
Dan Wilson <dan@thedatacollective.com.au>
2022-11-18 22:44:03 +1000

    update qmd

--- R/plot_palette.R
+++ R/plot_palette.R
@@ -10,13 +10,18 @@
 #' @export
 plot_palette <- function(
   palette = c("#0077b6", "#e88d67", "#ea638c", "#92dce5", "#f7ec59", "#ff66d8", "#2b2d42", "#f8f7f9"),
-  theme = theme_plot(),
-  seed = runif(1, 1, 1000)
+  theme = "default",
+  seed = 2022
   ) {
 
+    doc <- system.file(
+      path = "qmd",
+      pattern = "plot_palette.qmd",
+      package = "plotcolr")
+
     quarto::quarto_render(
-      "plot_palette.qmd",
-      execute_params = c("palette" = palette, "theme" = theme, "seed" = seed))
+      doc,
+      execute_params = list("palette" = palette, "theme" = theme, "seed" = seed))
 
   }
 
